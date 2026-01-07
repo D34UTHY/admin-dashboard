ca79fd4a0a365ca4688232e695ea09036d7ff63c
Matheus Sant'Ana <98056689+D34UTHY@users.noreply.github.com>
2026-01-07 19:41:41 -0300

    Adicionei os arquivos do projeto que finalizei

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Tutorial de JavaScript',
+        productNumber: '85743',
+        paymentStatus: 'Débito',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Curso Completo Web Dev',
+        productNumber: '97245',
+        paymentStatus: 'Reembolsado',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Curso de OWASP',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
